82c5a212005113d94984f24a24f6bfc3e5175fe7
Shingai-220424 <OCA2204240009@edu.oca.ac.jp>
2024-11-09 14:03:33 +0900

    リファクタリング演習2

--- refactoring.py
+++ refactoring.py
@@ -1,3 +1,5 @@
+import re
+
 def sum():
     result = 0
     for i in range(1, 11):
@@ -6,3 +8,15 @@ def sum():
 
 def test_sum():
     assert sum() == 55
+
+def repeat_string(s, num1, num2):
+    if not (5 <= len(s) <= 10):
+        raise ValueError("文字列は5文字以上、10以下でなければなりません")
+    if not re.match("^[a-zA-z]+$", s):
+        raise ValueError("文字列はアルファベットのみでなければなりません")
+    horizontal = s * num1
+
+    result =  (horizontal + "\n") * num2
+
+    return result.rstrip("\n")
+
